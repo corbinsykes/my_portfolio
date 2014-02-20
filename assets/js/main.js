5b28ba966eff2048fb2aaaf8732bdd74756e98e8
Corbin Sykes <corbin.sykes@gmail.comm>
2014-02-20 16:58:54 -0500

    next iteration

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,24 @@
 $(document).ready(function() {
+  // initializing stellar.js
   $(window).stellar();
 
+  // defining the bottom of the top section
   var mainbottom = $('#home').offset().top + $('#home').height();
-
   // on scroll,
   $(window).on('scroll',function(){
-
-      // we round here to reduce a little workload
-      stop = Math.round($(window).scrollTop());
-      if (stop > mainbottom) {
-          $('.navbar').removeClass('navbar-default');
-          $('.navbar').addClass('navbar-inverse');
-      } else {
-          $('.navbar').removeClass('navbar-inverse');
-          $('.navbar').addClass('navbar-default');
-      }
-
+    // we round here to reduce a little workload
+    stop = Math.round($(window).scrollTop());
+    if (stop > mainbottom) {
+        $('.navbar').removeClass('navbar-inverse');
+        $('.navbar').addClass('navbar-default');
+    } else {
+        $('.navbar').removeClass('navbar-default');
+        $('.navbar').addClass('navbar-inverse');
+    }
   });
 
   $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
+    $('a[href*=#]:not([href=#bio-carousel])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
